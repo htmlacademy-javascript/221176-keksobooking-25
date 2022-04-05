980bc96d6b63f0bdcea27ecc2bcc9259deca0418
Aleksandr Tarasyuk <alextarasyuk@fastmail.com>
2022-04-05 10:46:15 +0700

    Загрузка данных с сервера

--- js/forms.js
+++ js/forms.js
@@ -2,6 +2,8 @@ import { PRICES } from './settings.js';
 import { setSliderState } from './slider.js';
 import './validation.js';
 
+const ALERT_SHOW_TIME = 5000;
+
 const FORM_CLASS_NAMES = ['ad-form', 'map__filters'];
 
 const typeElement = document.querySelector('#type');
@@ -9,6 +11,16 @@ const priceElement = document.querySelector('#price');
 const timeInElement = document.querySelector('#timein');
 const timeOutElement = document.querySelector('#timeout');
 const addressElement = document.querySelector('#address');
+
+/*
+  const successMessageTemplate = document.querySelector('#success')
+  .content
+  .querySelector('.success');
+
+const errorMessageTemplate = document.querySelector('#error')
+  .content
+  .querySelector('.error');
+*/
 
 const setStateToAttrDisabled = (element, state) => {
   for (let i = 0; i < element.children.length; i++) {
@@ -62,9 +74,40 @@ const setAddress = (location) => {
   addressElement.value = `${location.lat.toFixed(5)}, ${location.lng.toFixed(5)}`;
 };
 
+/*
+const showMessage = () => {
+  const message = errorMessageTemplate.cloneNode(true);
+  document.body.append(message);
+  setTimeout(() => {
+    message.remove();
+  }, ALERT_SHOW_TIME);
+}
+*/
+
+const showMessage = () => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.color = 'white';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = 'Ошибка загрузки похожих объявления';
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
 
 typeElement.addEventListener('change', onTypeChange);
 timeInElement.addEventListener('change', onTimeChange);
 timeOutElement.addEventListener('change', onTimeChange);
 
-export {setFormsState, setAddress };
+export {setFormsState, setAddress, showMessage };
--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-import { createMarker } from './map.js';
-import { makeOffersArray } from './data.js';
 import { createOfferPopup } from './offers-markup.js';
+import { createMarker } from './map.js';
+import { getData } from './api.js';
+import { showMessage } from './forms.js';
 
-const offers = makeOffersArray(10);
+const MAX_SIMILAR_OFFERS = 10;
 
-offers.forEach((offer) => {
-  createMarker(offer.location, createOfferPopup(offer));
-});
+getData((offers) => {
+  offers.slice(0, MAX_SIMILAR_OFFERS).forEach((offer) => {
+    createMarker(offer.location, createOfferPopup(offer));
+  });
+}, showMessage);
